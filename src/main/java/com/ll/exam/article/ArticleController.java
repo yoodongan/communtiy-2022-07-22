cd3bb560cbb0737d66453daaee044d17fc307003
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-22 21:31:58 +0900

    게시물 객체 3개 구현

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -2,8 +2,21 @@ package com.ll.exam.article;
 
 import com.ll.exam.Rq;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArticleController {
     public void showList(Rq rq) {
         rq.appendBody("게시물 입니다.");
+        List<ArticleDto> articleDocs = new ArrayList<>();
+        articleDocs.add(new ArticleDto(5, "제목5", "내용5"));
+        articleDocs.add(new ArticleDto(4, "제목4", "내용4"));
+        articleDocs.add(new ArticleDto(3, "제목3", "내용3"));
+        articleDocs.add(new ArticleDto(2, "제목2", "내용2"));
+        articleDocs.add(new ArticleDto(1, "제목1", "내용1"));
+
+        rq.setAttr("articles", articleDocs);
+        rq.view("usr/article/list");
+
     }
 }
---
+++ src/main/java/com/ll/exam/article/ArticleDto.java
@@ -0,0 +1,46 @@
+package com.ll.exam.article;
+
+public class ArticleDto {
+    private int id;
+    private String title;
+    private String body;
+
+    public ArticleDto(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticeDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
