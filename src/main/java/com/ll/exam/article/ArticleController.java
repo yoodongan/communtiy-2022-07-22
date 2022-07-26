b9925e893d7b09e4fc55ad4498654d9c1d15577f
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-26 22:35:53 +0900

    form 으로 등록한 게시물들 나오도록 구현.

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -15,14 +15,8 @@ public class ArticleController {
 
     public void showList(Rq rq) {
         rq.appendBody("게시물 입니다.");
-        List<ArticleDto> articleDocs = new ArrayList<>();
-        articleDocs.add(new ArticleDto(5, "제목5", "내용5"));
-        articleDocs.add(new ArticleDto(4, "제목4", "내용4"));
-        articleDocs.add(new ArticleDto(3, "제목3", "내용3"));
-        articleDocs.add(new ArticleDto(2, "제목2", "내용2"));
-        articleDocs.add(new ArticleDto(1, "제목1", "내용1"));
-
-        rq.setAttr("articles", articleDocs);
+        List<ArticleDto> articleDtos = articleService.findAll();  // Service 에서 리스트로 가져온다.
+        rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
 
     }
@@ -37,7 +31,7 @@ public class ArticleController {
 
         long id = articleService.write(title, body);
 
-        rq.appendBody("%d번 게시물이 저장되었습니다.\n".formatted(id));
+        rq.appendBody("%d번 게시물이 저장되었습니다.  ".formatted(id));
         rq.appendBody("제목 : %s, 내용 : %s".formatted(title, body));
 
     }
--- src/main/java/com/ll/exam/article/ArticleRepository.java
+++ src/main/java/com/ll/exam/article/ArticleRepository.java
@@ -21,4 +21,8 @@ public class ArticleRepository {
 
         return(id);
     }
+
+    public List<ArticleDto> findAll() {
+        return datum;
+    }
 }
